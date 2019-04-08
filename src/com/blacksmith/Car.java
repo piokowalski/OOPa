8b2d71ce0bd621e62e7c42367751961d7b0ce01a
piokowalski <piokowalski@onet.pl>
2019-04-08 22:27:37 +0200

    Cars objects and Main class created

--- src/com/blacksmith/Car.java
+++ src/com/blacksmith/Car.java
@@ -2,4 +2,23 @@ package com.blacksmith;
 
 public class Car {
 
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public String setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("porsche") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+            return this.model;
+        }
+
+        public String getModel() {
+        return this.model;
+    }
 }
